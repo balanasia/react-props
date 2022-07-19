28168897497cbb2d7c5aa90c8e6dddab49fffb92
Anastasiia <41988936+balanasia@users.noreply.github.com>
2022-07-19 13:02:58 -0500

    Organize the code into imports and exports to clean out the code

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Luz Noceda",
+    img:
+      "https://ucarecdn.com/1c255ef7-533e-4f74-8ada-e403d5c5d959/-/progressive/yes/-/format/auto/",
+    tel: "+123 456 7890",
+    email: "luz@toh.com"
+  },
+  {
+    name: "Eda Clawthorne",
+    img:
+      "https://i.pinimg.com/564x/69/8d/4c/698d4cfe112127e62b58dee95820c8c2.jpg",
+    tel: "+123 456 7890",
+    email: "owllady@toh.com"
+  },
+  {
+    name: "Hooty",
+    img:
+      "https://steamuserimages-a.akamaihd.net/ugc/1702911301182996387/14681D67DA4533E5DEBE7F4A1C5A3647610E5092/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
+    tel: "+123 456 7890",
+    email: "hoothoot@toh.com"
+  }
+];
+
+export default contacts;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-function Card(props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img src={props.img} width="60%" alt="avatar_img" />
-      <p>{props.tel}</p>
-      <p>{props.email}</p>
-    </div>
-  );
-}
+import App from "./components/App.jsx";
 
 ReactDOM.render(
   <div>
-    <h1>My Contacts</h1>
-
-    <Card
-      name="Luz Noceda"
-      img="https://ucarecdn.com/1c255ef7-533e-4f74-8ada-e403d5c5d959/-/progressive/yes/-/format/auto/"
-      tel="+123 456 7890"
-      email="luz@toh.com"
-    />
-
-    <Card
-      name="Eda Clawthorne"
-      img="https://i.pinimg.com/564x/69/8d/4c/698d4cfe112127e62b58dee95820c8c2.jpg"
-      tel="+123 456 7890"
-      email="owllady@toh.com"
-    />
-    <Card
-      name="Hooty"
-      img="https://steamuserimages-a.akamaihd.net/ugc/1702911301182996387/14681D67DA4533E5DEBE7F4A1C5A3647610E5092/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
-      tel="+123 456 7890"
-      email="hoothoot@toh.com"
-    />
+    <App />
   </div>,
   document.getElementById("root")
 );
